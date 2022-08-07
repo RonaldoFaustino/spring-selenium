1e14d8fdb62e6436dcef00fea1d4acbdb6ab3231
RonaldoFaustino <ohnirucse@msn.com>
2022-08-07 13:05:56 -0300

    Dependency Injection With Spring Boot

---
+++ src/test/java/com/udemy/spring/springselenium/Address.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.springselenium;
+
+public class Address {
+    private String street;
+
+    public Address(){
+        this.street = "123 non Main st";
+    }
+
+    public String getStreet() {
+        return street;
+    }
+}
