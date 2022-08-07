f6ef8b68056844c5764b4ac8d3770a9c5243042b
RonaldoFaustino <ohnirucse@msn.com>
2022-08-07 15:07:34 -0300

    How To Autowire Objects From External Libraries

--- src/test/java/com/udemy/spring/springselenium/SpringSeleniumApplicationTests.java
+++ src/test/java/com/udemy/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -1,5 +1,6 @@
 package com.udemy.spring.springselenium;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +13,9 @@ class SpringSeleniumApplicationTests {
 
 	@Autowired
 	private User user;
+
+	@Autowired
+	private Faker faker;
 
 	@Value("${TEST_URL:http://www.google.com}")
 	private String url;
@@ -26,6 +30,8 @@ class SpringSeleniumApplicationTests {
 	void contextLoads() {
 		System.out.println(this.url);
 		this.user.printDetails();
+		Faker faker = new Faker();
+		System.out.println(faker.name().firstName() +" "+ faker.name().lastName());
 	}
 
 	/*Address address = new Address();
