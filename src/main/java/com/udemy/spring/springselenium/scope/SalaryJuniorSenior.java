8d5aa9766ccb380d4aa6ad33ba9be4192b6a0b64
RonaldoFaustino <ohnirucse@msn.com>
2022-09-13 21:05:48 -0300

    Prototype

--- src/main/java/com/udemy/spring/springselenium/scope/SalaryJuniorSenior.java
+++ src/main/java/com/udemy/spring/springselenium/scope/SalaryJuniorSenior.java
@@ -1,8 +1,10 @@
 package com.udemy.spring.springselenium.scope;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class SalaryJuniorSenior {
 
     private int amount;
