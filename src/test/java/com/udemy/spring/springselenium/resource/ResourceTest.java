33e2b8feb3acacab0df5ffa8d90328627538e28f
RonaldoFaustino <ohnirucse@msn.com>
2023-01-25 22:11:01 -0300

    Properties Utils

--- src/test/java/com/udemy/spring/springselenium/resource/ResourceTest.java
+++ src/test/java/com/udemy/spring/springselenium/resource/ResourceTest.java
@@ -2,18 +2,24 @@ package com.udemy.spring.springselenium.resource;
 
 import com.udemy.spring.springselenium.SpringBaseJunitTest;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
 public class ResourceTest extends SpringBaseJunitTest {
 
     @Value("classpath:data/testdata.csv")
     private Resource resource;
+
+    @Value("classpath:data/testdata2.csv")
+    private Resource resource2;
 
     @Value("file:C:\\Exemplo SpringBoot\\spring-selenium\\src\\test\\resources\\data\\testdata.csv")
     private Resource fileAbsolute;
@@ -26,6 +32,9 @@ public class ResourceTest extends SpringBaseJunitTest {
 
     @Value("${screenshot.path}/some.txt")
     private Path path;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @Test
     public void resourceTest() throws IOException {
@@ -54,4 +63,26 @@ public class ResourceTest extends SpringBaseJunitTest {
     public void resourceTest4() throws IOException {
         FileCopyUtils.copy(url2.getInputStream(), Files.newOutputStream(path));
     }
+
+    @Test
+    public void resourceTest5() throws IOException {
+        FileCopyUtils.copy(url2.getInputStream(), Files.newOutputStream(path));
+    }
+
+//    @Test(dataProvider = "getData")
+//    public void resourceTest(String url, String saveas) throws IOException {
+//        FileCopyUtils.copy(
+//                resourceLoader.getResource(url).getInputStream(),
+//                Files.newOutputStream(path.resolve(saveas));
+//        )
+//    }
+//
+//
+//    @DataProvider
+//    public Stream<String[]> getData() throws IOException {
+//        return Files.readAllLines(resource.getFile().toPath())
+//                .stream()
+//                .map(s -> s.split(",")) Stream<String[]>
+//                .toArray(Object[][]::new);
+//    }
 }
